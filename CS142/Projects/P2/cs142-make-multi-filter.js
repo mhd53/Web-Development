fe3c66288402030a456de04a46acfcd769035d23
mhd53 <mhd53@users.noreply.github.com>
2017-05-13 19:37:00 -0400

    Update

---
+++ CS142/Projects/P2/cs142-make-multi-filter.js
@@ -0,0 +1,34 @@
+// @mhd53
+// CS142 Project 2: JavaScript Calisthenics
+
+function cs142MakeMultiFilter(originalArray) {
+	/* Takes an array as a parameter and rerturns a function that can be used
+	   to filter the elements of this array.
+	   Parameters: Array
+	   Returns: function */
+
+
+
+}
+
+function arrayFilterer(filterCriteria, callback) {
+	/* Internally keeps track of a notion called currentArray. Initally currentArray is 
+	   set to be identical to originalArray. 
+	   Parameters: two functions
+	   Returns: Should return itself unless the filterCriteria parameter is not specified
+	   in which case it should return the currentArray. */
+
+
+}
+
+function filterCriteria(arrayElement) {
+	// Parameters: an array element
+	// Returns: a boolean
+
+
+}
+
+function callback(currentArray) {
+	// Returns: null
+
+}
