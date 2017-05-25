71147394a419e8a8efcf7d80b47fe0e215c329f8
mhd53 <mhd53@users.noreply.github.com>
2017-05-25 08:30:38 -0400

    Add filter

--- CS142/Projects/P2/cs142-make-multi-filter.js
+++ CS142/Projects/P2/cs142-make-multi-filter.js
@@ -1,3 +1,4 @@
+"use strict"
 // @mhd53
 // CS142 Project 2: JavaScript Calisthenics
 
@@ -5,30 +6,40 @@ function cs142MakeMultiFilter(originalArray) {
 	/* Takes an array as a parameter and rerturns a function that can be used
 	   to filter the elements of this array.
 	   Parameters: Array
-	   Returns: function */
+	   Returns: function 
+	*/
 
+	var arrayFilterer = function arrayFilterer(filterCriteria, callback) {
+		/* Internally keeps track of a notion called currentArray. Initally currentArray is 
+		   set to be identical to originalArray. 
+		   Parameters: two functions
+		   Returns: Should return itself unless the filterCriteria parameter is not specified
+		   in which case it should return the currentArray. 
+		*/
 
+		// Variables
+		var currentArray = originalArray;
 
+		if (typeof filterCriteria != "function") {
+			return currentArray;
+		}
+
+		for (var i = 0; i < originalArray.length; i++) {
+			var e = originalArray[i];
+			if (filterCriteria(e) == false) {
+				delete currentArray[i];
+			}
+		}
+
+		if (callback != undefined) {
+			callback();
+		}
+
+		return arrayFilterer;
+	}
+
+	return arrayFilterer;
 }
 
-function arrayFilterer(filterCriteria, callback) {
-	/* Internally keeps track of a notion called currentArray. Initally currentArray is 
-	   set to be identical to originalArray. 
-	   Parameters: two functions
-	   Returns: Should return itself unless the filterCriteria parameter is not specified
-	   in which case it should return the currentArray. */
-
-
-}
-
-function filterCriteria(arrayElement) {
-	// Parameters: an array element
-	// Returns: a boolean
-
-
-}
-
-function callback(currentArray) {
-	// Returns: null
-
-}
+alert("Hello World!");
+// console.log("Type of function: " + (typeof cs142MakeMultiFilter));
