66de1b6b8491a5c4eb1b196ddec0b7e777c3cef4
mhd53 <mhd53@users.noreply.github.com>
2018-07-05 12:30:51 -0400

    add tensor-app

---
+++ React/tensor-app/src/TensorComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import * as tf from '@tensorflow/tfjs';
+
+// Styled Components
+const StyledContainer = styled.div`
+	
+	width: ${props => props.width};
+
+	`; 
+
+// Main Components
+
---
+++ React/tensor-app/src/theme.js
@@ -0,0 +1,7 @@
+export default {
+	background: '#f5f8fb',
+	fontFamily: 'monospace',
+	buttonBgColor: 'edac2a',
+	buttonTexColor: 'ffffff',
+	buttonBdColor: 
+};
