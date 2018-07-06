5e4082e95eef7fce29380b4496edbd8cf7edbd7b
mhd53 <mhd53@users.noreply.github.com>
2018-07-06 11:50:10 -0400

    creating simpleNN

--- React/tensor-app/src/TensorComponent.js
+++ React/tensor-app/src/TensorComponent.js
@@ -24,6 +24,99 @@ StyledButton.defaultProps = {
 
 // Main Components
 
+class CreateData extends React.Compoent {
+	constructor(props) {
+	  super(props);
+	
+	  this.state = {data: []};
+	}
+
+	createSynData() {
+
+	}
+
+}
+
+class SimpleNN extends React.Component {
+	constructor(props) {
+	  super(props);
+	
+	  this.state = {};
+	}
+
+	createModel() {
+		var model = tf.sequential();
+		model.add(tf.layers.dense({units: 8, inputShape: 2, activation: 'tanh'}));
+		model.add(tf.layers.dense({units: 1, activation: 'sigmoid'}));
+		model.complile({optimizer: 'sgd', loss: 'binaryCrossentropy', lr:0.1});
+		return model;
+	}
+}
+
+const xs = tf.tensor2d([[0, 0], [0, 1], [1, 0], [1, 1]]);
+const ys = tf.tensor2d([[0], [1], [1], [0]]);
+
+class OptimizationModel extends React.Component {
+	constructor(props) {
+	  super(props);
+	
+	  this.state = {};
+	  this.f = this.f.bind(this);
+	  this.minimize = this.minimize.bind(this);
+	  this.runTest = this.runTest.bind(this);
+	}
+
+	/**
+	 * Function to minimize.
+	 * @param  {tensor} x -data
+	 * @return {tensor}  computation graph
+	 */
+	f(x) {
+		const f1 = x.pow(tf.scalar(6, 'int32')); // x^6
+		const f2 = x.pow(tf.scalar(4, 'int32')).mul(tf.scalar(2)); // 2x^4
+		const f3 = x.pow(tf.scalar(2, 'int32')).mul(tf.scalar(3));// 3x^2
+		const f4 = tf.scalar(1); // 1
+
+		return f1.add(f2).add(f3).add(x).add(f4);
+	}
+
+	/**
+	 * Run training on the model and minimize the value of 'x' which
+	 * in the context of ML, 'x' represents the training data.
+	 * Therefore, the objective of the optimization algorithm is not to 
+	 * minimize the loss of the model by finding optimal theta parameters 
+	 * which in the context of ML, theta represents the weight of each feature 
+	 * in the training dataset. Instead gradient descent is simply run on the
+	 * function f(x) and returnes the global min of the function. 
+	 *
+	 * @param  {[type]} epochs -number of iterations over the dataset
+	 * @param  {float} lr     -learning rate alpha
+	 * @return {[tensor]}     -global minimum of f(x)
+	 */
+	minimize(epochs, lr) {
+		let y = tf.variable(tf.scalar(2)) // initial value
+		const optim = tf.train.adam(lr) // gradient descent algorithm
+		for (let i = 0; i < epochs; i++) { // start minimization
+			optim.minimize(() => this.f(y));
+		}
+		return y;
+	}
+
+	runTest() {
+		this.minimize(200, 0.9).print();
+	}
+
+	render() {
+		return (
+			<StyledButton primary onClick={this.runTest}>
+				Run Optimzation
+			</StyledButton>
+			);
+	}
+
+
+}
+
 class TestLinearModel extends React.Component {
 	constructor(props) {
 	  super(props);
@@ -33,6 +126,8 @@ class TestLinearModel extends React.Component {
 	  // Bind methods`
 	  this.linearModel = this.linearModel.bind(this); 
 	  this.thisIsATest = this.thisIsATest.bind(this);
+	  this.f = this.f.bind(this);
+	  this.testFx = this.testFx.bind(this);
 	}
 
 	/**
@@ -68,7 +163,38 @@ class TestLinearModel extends React.Component {
 	thisIsATest() {
 		const tensor = tf.scalar(2);
 		const input = tf.tensor([2, 2]);
-		const tensor_s = input.shape.print();
+		const tensor_s = input.shape;
+
+		// Operators
+		const a = tf.tensor([1, 2, 3]);
+		a.square().print();
+		const a2 = a.square().square();
+		a2.print();
+		a.dispose();
+
+	}
+
+	testFx() {
+		const x = tf.tensor([1, 2, 3]);
+		this.f(x).print();
+	}
+
+	f(x) {
+		return tf.tidy(() => {
+			const y = x.square();
+			const z = x.mul(y);
+			return z;
+		});
+	}
+
+	polyF(x) {
+		// Optimization problem
+		const f1 = x.pow(tf.scalar(6, 'int32')) // x^6
+		const f2 = x.pow(tf.scalar(4, 'int32')).mul(tf.scalar(2)) // 2x^4
+		const f3 = x.pow(tf.scalar(2, 'int32')).mul(tf.scalar(3)) // 3x^2
+		const f4 = tf.scalar(1) // 1
+
+		return f1.add(f2).add(f3).add(x).add(f4)
 	}
 
 	render() {
@@ -76,20 +202,30 @@ class TestLinearModel extends React.Component {
 			<StyledContainer>
 				<Container>
 					<Row>
-						<Col xs="6">
+						<Col xs="4">
 							<StyledButton primary onClick={this.linearModel}>
 								Predict
 							</StyledButton>
 						</Col>
+						<Col xs="4">
+							<StyledButton onClick={this.thisIsATest}>
+								Testing
+							</StyledButton>
+						</Col>
 
+						<Col xs="4">
+							<StyledButton onClick={this.testFx}>
+								F(X)
+							</StyledButton>
+						</Col>
+					</Row>
+
+					<Row>
 						<Col xs="6">
-
+							<OptimizationModel />
 						</Col>
 					</Row>
 				</Container>
-				<StyledButton onClick={this.thisIsATest}>
-					Testing
-				</StyledButton>
 			</StyledContainer>
 			);
 	}
